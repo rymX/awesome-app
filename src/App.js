2879e0bb48a09ff7225848cc0da39c106792f1e6
rym fr <ryymfradi@gmail.com>
2022-09-24 14:06:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import './App.css';
 
+
 function App() {
+
+  const [apis, setapis] = useState([]);
+
+const fetchApis = async ()=>{
+  const result = await axios(
+    'https://api.publicapis.org/entries',
+  );
+  console.log(result.data.entries[0])
+  setapis(result);
+}
+
+  useEffect(() => {
+    fetchApis();   
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
     </div>
   );
 }
