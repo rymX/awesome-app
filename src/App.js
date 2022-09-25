6ecbd15570e05bf7bb58e546ab520572c3a7ce88
rym fr <ryymfradi@gmail.com>
2022-09-25 18:55:35 +0100

    pagination done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Breadcrumb, Layout, Menu, Card, Col, Row, List } from "antd";
+import ReactPaginate from "react-paginate";
 import "./App.css";
 
 const { Header, Content, Footer } = Layout;
@@ -8,14 +9,59 @@ const { Header, Content, Footer } = Layout;
 const App = () => {
   const [apis, setApis] = useState([]);
 
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+
+  const [itemOffset, setItemOffset] = useState(0);
+
   const fetchApis = async () => {
     const apis = await axios("https://api.publicapis.org/entries");
-    setApis(apis);
+    setApis(apis.data.entries);
+
+    const endOffset = itemOffset + 20;
+
+    setCurrentItems(apis.data.entries.slice(itemOffset, endOffset));
+
+    setPageCount(Math.ceil(apis.data.entries.length / 20));
   };
 
   useEffect(() => {
     fetchApis();
-  }, []);
+  }, [itemOffset]);
+
+  function Items({ currentItems }) {
+    return (
+      <>
+        {currentItems &&
+          currentItems.map((item) => (
+            <div>
+               <Card
+                    size="small"
+                    extra={<a href="#">More</a>}
+                    title={"test"}
+                    bordered={false}
+                    style={{
+                      width: 250,
+                      margin: 15,
+                    }}
+                  >
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                  </Card>
+            </div>
+          ))}
+      </>
+    );
+  }
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * 20) % apis.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
 
   return (
     <div className="App">
@@ -25,18 +71,13 @@ const App = () => {
             position: "fixed",
             zIndex: 1,
             width: "100%",
+            theme:"dark",
+            mode:"horizontal"
           }}
         >
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            items={new Array(3).fill(null).map((_, index) => ({
-              key: String(index + 1),
-              label: `nav ${index + 1}`,
-            }))}
-          />
+
+          
+         
         </Header>
         <Content
           className="site-layout "
@@ -45,7 +86,7 @@ const App = () => {
             marginTop: 64,
           }}
         >
-          <Breadcrumb
+          {/* <Breadcrumb
             style={{
               margin: "16px 0",
             }}
@@ -53,44 +94,43 @@ const App = () => {
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          {apis.data ? (
-            <div
-              className="site-layout-background site-card-border-less-wrapper"
-              style={{
-                padding: 24,
-                minHeight: 380,
-              }}
-            >
-              <Row
-                grid={{
-                  gutter: 16,
-                  xs: 1,
-                  sm: 2,
-                  md: 4,
-                  lg: 4,
-                  xl: 6,
-                  xxl: 3,
+          </Breadcrumb> */}
+          {apis.length ? (
+            <>
+              <div
+                className="site-layout-background site-card-border-less-wrapper"
+                style={{
+                  padding: 24,
+                  minHeight: 380,
                 }}
               >
-                <Card
-                  size="small"
-                  extra={<a href="#">More</a>}
-                  title={apis.data.entries[0]["API"]}
-                  bordered={false}
-                  style={{
-                    width: 250,
-                    margin: 15,
+                <Row
+                  grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 4,
+                    lg: 4,
+                    xl: 6,
+                    xxl: 3,
                   }}
                 >
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-                </Card>
-               
-              </Row>
-            </div>
+                  <Items currentItems={currentItems} />
+                  <ReactPaginate
+                  breakLabel="..."
+                  nextLabel="next >"
+                  onPageChange={handlePageClick}
+                  pageRangeDisplayed={5}
+                  pageCount={pageCount}
+                  previousLabel="< previous"
+                  renderOnZeroPageCount={null}
+                />
+                </Row>
+                
+              </div>
+            </>
           ) : (
+          
             <div
               className="site-layout-background site-card-border-less-wrapper"
               style={{
@@ -124,35 +164,15 @@ const App = () => {
                   <p>Card content</p>
                   <p>Card content</p>
                 </Card>
-              
               </Row>
             </div>
           )}
-        </Content>  <Card
-                  size="small"
-                  extra={<a href="#">More</a>}
-                  title=""
-                  bordered={false}
-                  style={{
-                    width: 250,
-                    margin: 15,
-                  }}
-                  loading="true"
-                >
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-                </Card>
+        </Content>
         <Footer
           style={{
             textAlign: "center",
           }}
         >
-          {apis.data ? (
-            <h1>{apis.data.entries[0]["API"]}</h1>
-          ) : (
-            <h1>spinner</h1>
-          )}
           Ant Design ©2018 Created by Ant UED
         </Footer>
       </Layout>
--- src/layout.js
+++ src/layout.js
@@ -1,53 +1,151 @@
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card, Skeleton, Switch } from 'antd';
-import React, { useState } from 'react';
-const { Meta } = Card;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Breadcrumb, Layout, Menu, Card, Col, Row, List } from "antd";
+import ReactPaginate from 'react-paginate';
+import "./App.css";
+
+const { Header, Content, Footer } = Layout;
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  const [apis, setApis] = useState([]);
 
-  const onChange = (checked) => {
-    setLoading(!checked);
+  const fetchApis = async () => {
+    const apis = await axios("https://api.publicapis.org/entries");
+    setApis(apis);
   };
 
+  useEffect(() => {
+    fetchApis();
+  }, []);
+
   return (
-    <>
-      <Switch checked={!loading} onChange={onChange} />
-
-      <Card
-        style={{
-          width: 300,
-          marginTop: 16,
-        }}
-        loading={loading}
-      >
-        <Meta
-          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-          title="Card title"
-          description="This is the description"
-        />
-      </Card>
-
-      <Card
-        style={{
-          width: 300,
-          marginTop: 16,
-        }}
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Skeleton loading={loading} avatar active>
-          <Meta
-            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-            title="Card title"
-            description="This is the description"
+    <div className="App">
+      <Layout>
+        <Header
+          style={{
+            position: "fixed",
+            zIndex: 1,
+            width: "100%",
+          }}
+        >
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            items={new Array(3).fill(null).map((_, index) => ({
+              key: String(index + 1),
+              label: `nav ${index + 1}`,
+            }))}
           />
-        </Skeleton>
-      </Card>
-    </>
+        </Header>
+        <Content
+          className="site-layout "
+          style={{
+            padding: "0 50px",
+            marginTop: 64,
+          }}
+        >
+          {/* <Breadcrumb
+            style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb> */}
+          {apis.data ? (
+            <div
+              className="site-layout-background site-card-border-less-wrapper"
+              style={{
+                padding: 24,
+                minHeight: 380,
+              }}
+            >
+              <Row
+                grid={{
+                  gutter: 16,
+                  xs: 1,
+                  sm: 2,
+                  md: 4,
+                  lg: 4,
+                  xl: 6,
+                  xxl: 3,
+                }}
+              >
+                {apis.data.entries.map((api) => {
+                  return (
+                    <Card
+                      size="small"
+                      extra={<a href="#">More</a>}
+                      title={api["API"]}
+                      bordered={false}
+                      style={{
+                        width: 250,
+                        margin: 15,
+                      }}
+                    >
+                      <p>Card content</p>
+                      <p>Card content</p>
+                      <p>Card content</p>
+                    </Card>
+                  );
+                })}
+              </Row>
+            </div>
+          ) : (
+            <div
+              className="site-layout-background site-card-border-less-wrapper"
+              style={{
+                padding: 24,
+                minHeight: 380,
+              }}
+            >
+              <Row
+                grid={{
+                  gutter: 16,
+                  xs: 1,
+                  sm: 2,
+                  md: 4,
+                  lg: 4,
+                  xl: 6,
+                  xxl: 3,
+                }}
+              >
+                <Card
+                  size="small"
+                  extra={<a href="#">More</a>}
+                  title=""
+                  bordered={false}
+                  style={{
+                    width: 250,
+                    margin: 15,
+                  }}
+                  loading="true"
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </Row>
+            </div>
+          )}
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          {apis.data ? (
+            <h1>{apis.data.entries[0]["API"]}</h1>
+          ) : (
+            <h1>spinner</h1>
+          )}
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </div>
   );
 };
 
